2069f3894f31b0fcebc546d7ae6218b2b3c2ef69
Gokkull04 <gokkull04@gmail.com>
2024-08-08 22:27:59 +0530

    fonts

--- src/pages/Invoice.js
+++ src/pages/Invoice.js
@@ -87,16 +87,10 @@ const Invoice = () => {
             </div>
           </div>
 
-          <div className="text-center font-bold mb-6 mt-24  text-7xl font-playfair">
-            <span className=''>T</span>
-            <span className=''>H</span>
-            <span className=''>A</span>
-            <span className=''>N</span>
-            <span className=''>K</span>
-            <span className='px-3'> </span>
-            <span className=''>Y</span>
-            <span className=''>O</span>
-            <span className=''>U</span>
+          <div className="font-bold mb-6 mt-24  text-7xl font-playfair flex justify-center">
+            <p>THANK</p>
+            <span className='px-3'></span>
+            <p>YOU</p>
           </div>
 
           <div className="text-left mt-16">
